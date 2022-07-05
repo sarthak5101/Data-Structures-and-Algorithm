5933f9e89f4bf1638637c53d80c2aede2dd1a02a
Sarthak Sharma <89703198+sarthak5101@users.noreply.github.com>
2022-07-06 01:10:25 +0530

    Add files via upload

---
+++ chocolateDistribution.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findMinDiff(int arr[], int n, int m) {
+    // If there are no chocolate or the number of students is 0
+    if (m == 0 || n == 0) 
+        return 0;
+
+    // Sort the given packets
+    sort(arr, arr + n);
+
+    // Number of students cannot be more than number of packets
+    if (n < m) 
+        return -1;
+
+    int min_diff = INT_MAX;
+
+    // Find the subarray of size m such that difference between last (maximum in case of sorted)
+    // and first (minimum in case of sorted) elements of subarray is minimum.
+
+    for (int i = 0; i + m - 1 < n; i++) {
+        int diff = arr[i + m - 1] - arr[i];
+        if (diff < min_diff)
+            min_diff = diff;
+    }
+    return min_diff;
+}
+
+int main() {
+     int arr[] = { 12, 4,  7,  9,  2,  23, 25, 41, 30, 40, 28, 42, 30, 44, 48, 43, 50 };
+     int m = 7; // Number of students
+     int n = sizeof(arr)/sizeof(arr[0]);
+     cout << "Minimum difference is " << findMinDiff(arr, n, m);
+     return 0;
+}
