c5d5051297cbc0567a9d40b95e4869b17554449b
Sarthak Sharma <89703198+sarthak5101@users.noreply.github.com>
2022-07-10 23:53:48 +0530

    Add files via upload

---
+++ rotationOfEachOther.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool areRotations(string str1, string str2) {
+    if(str1.length() != str1.length())
+        return false;
+
+    string temp = str1 + str1;
+    return (temp.find(str2) != string::npos);
+}
+
+int main() {
+     string str1 = "AACD", str2 = "ACDA";
+   if (areRotations(str1, str2))
+       cout << "Strings are rotations of each other" << endl;
+   else
+       cout << "Strings are not rotations of each other" << endl;
+    return 0;
+}
