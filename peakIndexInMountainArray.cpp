c8bd0a13f178f97e7b5f6eadc04521f862f172ea
Sarthak Sharma <89703198+sarthak5101@users.noreply.github.com>
2022-07-21 17:16:13 +0530

    Add files via upload

---
+++ peakIndexInMountainArray.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int peakIndexInMountainArray(vector<int>& arr) {
+    int s = 0;
+    int e = arr.size() - 1;
+    int mid = s + (e - s)/2;
+
+    while (s < e) {
+        if (arr[mid] < arr[mid + 1]) {
+            s = mid + 1;
+        }
+        else {
+            e = mid;
+        }
+        mid = s + (e - s)/2;
+    }
+    return s;
+}
+
+int main () {
+    vector<int> arr = {0, 10, 5, 2};
+     
+     cout << peakIndexInMountainArray(arr) << endl;
+     return 0;
+}
