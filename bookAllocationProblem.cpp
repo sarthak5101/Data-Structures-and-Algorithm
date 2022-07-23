25bfd27c9817c7a6bffd5d9d08d687d1b07f2932
Sarthak Sharma <89703198+sarthak5101@users.noreply.github.com>
2022-07-23 20:00:58 +0530

    Add files via upload

---
+++ bookAllocationProblem.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int isPossible(vector<int> arr, int n, int m, int mid) {
+    int studentCount = 1;
+    int pageSum = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (pageSum + arr[i] <= mid) {
+            pageSum +=  arr[i];
+        }
+        else {
+            studentCount++;
+            if (studentCount > m || arr[i] > mid) {
+                return false;
+            }
+            pageSum = arr[i];
+        }
+    }
+    return true;
+}
+
+int allocateBooks (vector<int> arr, int n, int m) {
+    int s = 0;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    int e = sum;
+    int mid = s + (e - s)/2;
+    int ans = -1;
+
+    while (s <= e) {
+        if (isPossible(arr, n, m, mid)) {
+            ans = mid;
+            e = mid - 1;
+        }
+        else {
+            s = mid + 1;
+        }
+        mid = s + (e -s)/2;
+    }
+    return ans;
+}
+
+int main () {
+    vector<int> arr = {12, 34, 67, 90};
+    int n = 4;
+    int m = 2;
+
+    cout << allocateBooks(arr, n, m) << " ";
+    return 0;
+}
