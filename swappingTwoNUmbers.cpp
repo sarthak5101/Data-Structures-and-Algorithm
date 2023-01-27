affbcb36dd3a1d66dbe66c400141da95a5de9380
Sarthak Sharma <89703198+sarthak5101@users.noreply.github.com>
2023-01-27 16:08:52 +0530

    Add files via upload

---
+++ swappingTwoNUmbers.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Driver code
+int main()
+{
+    int a = 2, b = 3;
+
+    cout << "Before swapping a = " << a << " , b = " << b << endl;
+
+    b = a + b;
+    a = b - a;
+    b = b - a;
+
+    cout << "After swapping a = " << a << " , b = " << b << endl;
+    return 0;
+}
